e18b62ad6b5788763cb60dfee2944026e4c89ff9
mpume <mpumeh5@yahoo.com>
2023-07-25 14:20:42 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strien(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a,  int n);
+void *_strcpy(char *dest, char *src);
+#endif
+
+
